b5f86f23172e3610e09780b57b16133de5ce4569
Salvador Guzman <guzmansalv@gmail.com>
2017-07-14 21:14:21 -0700

    added basic loop, snake, background and input

--- snake.js
+++ snake.js
@@ -7,8 +7,6 @@ const keyDown$ = keysUniq$.filter(s => s === 40).mapTo([0,1]);
 const keyLeft$ = keysUniq$.filter(s => s === 37).mapTo([-1,0]);
 const keyRight$ = keysUniq$.filter(s => s === 39).mapTo([1,0]);
 
-const keyInput$ = keyUp$.merge(keyDown$).merge(keyLeft$).merge(keyRight$).subscribe(s => direction = s);
-
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -16,13 +14,13 @@ const bgColor = 'black';
 
 const moveUnits = 1;
 const waitTimeBetweenFrames = 20;
-const widthOfSnake = 10;
+const widthOfSnake = 5;
 
 function rgbFromValues(red, green, blue) {
     return `rgb(${red},${green},${blue})`;
 }
 
-function square(_x, _y, _r = 5) {
+function square(_x, _y, _r = widthOfSnake) {
     return {
         x: _x,
         y: _y,
@@ -45,14 +43,23 @@ const initY = canvas.height / 2;
 let snake = square(initX, initY);
 let direction = [0,-1];
 
-const gameLoop$ = Rx.Observable.interval(waitTimeBetweenFrames).subscribe(() => {
+const keyInput$ = keyUp$.merge(keyDown$).merge(keyLeft$).merge(keyRight$).subscribe(s => direction = s);
+
+const gameLoop$ = Rx.Observable.interval(waitTimeBetweenFrames);
+
+const overflowRight$ = gameLoop$.map(s => snake.x > canvas.width).distinctUntilChanged().filter(s => s).subscribe(s => snake.x = 0);
+const overflowLeft$ = gameLoop$.map(s => snake.x < 0).distinctUntilChanged().filter(s => s).subscribe(s => snake.x = canvas.width);
+const overflowUp$ = gameLoop$.map(s => snake.y < 0).distinctUntilChanged().filter(s => s).subscribe(s => snake.y = canvas.height);
+const overflowDown$ = gameLoop$.map(s => snake.y > canvas.height).distinctUntilChanged().filter(s => s).subscribe(s => snake.y = 0);
+
+gameLoop$.subscribe(() => {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     context.fillStyle = bgColor;
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    snake.x += direction[0];
-    snake.y += direction[1];
+    snake.x += direction[0] * moveUnits;
+    snake.y += direction[1] * moveUnits;
 
     snake.render();
 });
